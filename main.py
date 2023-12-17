1b53b3144eeae31e5b4ffc0b1d1d355ec939b270
Assassin <moinak.dey8@gmail.com>
2023-12-17 23:45:57 +0530

    Modify the Reader class

--- main.py
+++ main.py
@@ -3,18 +3,40 @@ from os.path import isdir, join
 
 class Reader:
     totalLines = 0
+    skip_pop_up = False
+    files_to_ignore = []
 
     def start_reading(self, path):
         if not isdir(path):
             # no need to list files
-            self.read_lines(path)
+            self.read_lines(path, path)
             return
 
-        print("\n Listing all the files and subdirs under current directory...")
+        # print("\nListing all the files and subdirs under current directory...")
         list_ = listdir(path)
-        print(list_)
+        print("\nFound "+str(len(list_))+" file(s) under present directory!")
 
+        count = 1
+        for f in list_:
+            print(str(count)+". "+f)
+            count = count + 1
+
+        # ask for the filenames to ignore
+        if not self.skip_pop_up:
+            files_to_ignore_ = input("Mention which files to ignore(you can write SKIP to remove this popup forever): ")
+            if files_to_ignore_:
+                if files_to_ignore_ != "SKIP":
+                    file_nums = files_to_ignore_.split(" ")
+                    for idx in file_nums:
+                        self.files_to_ignore.append(list_[int(idx)-1])
+                else:
+                    self.skip_pop_up =  True
+                    
         for c in list_:
+            if c in self.files_to_ignore:
+                print("Ignoring  "+c)
+                continue
+
             if isdir(join(path, c)):
                 current_path = join(path, c)
                 print("\n"+ c + " is a directory!")
@@ -23,16 +45,17 @@ class Reader:
                 continue
 
             # if it is a file, read the total lines of code
-            print(c + " is a file...reading the total lines...")
+            print("\n"+c + " is a file...reading the total lines...")
             filepath = join(path, c)
-            self.read_lines(filepath)
+            self.read_lines(filepath, c)
 
-    def read_lines(self, filepath):
+    def read_lines(self, filepath, filename):
+
         with open(filepath, encoding="utf-8") as f:
             content = f.read()
             splits = content.split("\n")
             lines = len(splits) - 1
-            print("No. of lines in the current file is: "+str(lines))
+            print("No. of lines in "+filename+" is: "+str(lines))
             self.totalLines = self.totalLines + lines
 
 
